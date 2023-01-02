89e6d0f3acff4cf391899142b3caa33498997d5c
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-02 21:14:05 +0000

    install font via Next-document & set as deFault

---
+++ pages/_document.jsx
@@ -0,0 +1,25 @@
+import Document, { Html as HTML, Head, Main, NextScript } from 'next/document';
+
+class Document2 extends Document {
+	render() {
+		return (
+			<HTML>
+				<Head>
+					<meta name='title' content='NatureFlix' />
+					<meta name='description' content='Watch Videos to Admire the Beauty of Nature' />
+					<title>NatureFlix</title>
+					<link rel='icon' type='image/x-icon' href='/favicon.ico' />
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+					<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</HTML>
+		);
+	};
+};
+
+export default Document2;
