ad1e597258d67559c9546fee4bd55151d56db297
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-04 16:35:14 +0000

    validate eMail

--- pages/logIn.jsx
+++ pages/logIn.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Logo from '../components/logo/Logo';
 import styles from '../styles/logIn.module.css';
 
 function logIn() {
+	const [ eMail, setEMail ] = useState('');
+	const [ eMailValidity, setEMailValidity ] = useState(false);
+	const router = useRouter();
+
+	const onChange = ({ target }) => {
+		setEMail(target.value);
+		setEMailValidity(target.validity.valid);
+	};
+
 	const onClick = event => {
 		event.preventDefault();
 
-		console.log('CLICK');
+		if (eMailValidity) {
+			router.push('/');
+		};
 	};
 
 	return (
@@ -23,8 +36,8 @@ function logIn() {
 				<main className={styles.main}>
 					<form className={styles.form}>
 						<h1 className={styles.heading}>Log In</h1>
-						<input className={styles.input} type='email' placeholder='EMail' />
-						<pre className={styles.error}></pre>
+						<input className={styles.input} type='email' placeholder='EMail' onChange={onChange} />
+						{!eMailValidity && eMail && <pre className={styles.error}>InValid EMail Address</pre>}
 						<button className={styles.button} type='submit' onClick={onClick}>Log In</button> 
 					</form>
 				</main>
