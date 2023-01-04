6c0f52f194acf11d4f10ad0d12682818a78c98c9
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-04 15:38:10 +0000

    add log-in form, style log-in--page

--- pages/logIn.jsx
+++ pages/logIn.jsx
@@ -3,16 +3,32 @@ import Logo from '../components/logo/Logo';
 import styles from '../styles/logIn.module.css';
 
 function logIn() {
+	const onClick = event => {
+		event.preventDefault();
+
+		console.log('CLICK');
+	};
+
 	return (
 		<>
 			<Head>
 				<title>Log In | NatureFlix</title>
 			</Head>
-			<main>
-				<header className={styles.header}>
-					<Logo />
+			<div className={styles.container}>
+				<header>
+					<nav className={styles.navigation}>
+						<Logo />
+					</nav>
 				</header>
-			</main>
+				<main className={styles.main}>
+					<form className={styles.form}>
+						<h1 className={styles.heading}>Log In</h1>
+						<input className={styles.input} type='email' placeholder='EMail' />
+						<pre className={styles.error}></pre>
+						<button className={styles.button} type='submit' onClick={onClick}>Log In</button> 
+					</form>
+				</main>
+			</div>
 		</>
 	);
 };
