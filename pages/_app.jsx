7ffb1b6f251f2851efe280bc69bf75d47f568bd2
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-06 21:50:23 +0000

    detect authentication-state and route accordingly, show loading-text

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import magic from '../lib/magic';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	const [ loading, setLoading ] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const loggedIn = await magic.user.isLoggedIn();
+
+				if (loggedIn) {
+					router.push('/');
+				} else {
+					router.push('/logIn');
+				};
+			} catch (error) {
+				setLoading(false);
+				console.error('Error Detecting LogIn State', error);
+			};
+		})();
+	}, []);
+
+	useEffect(() => {
+		const unSetLoading = () => {setLoading(false)};
+
+		router.events.on('routeChangeComplete', unSetLoading);
+		router.events.on('routeChangeError', unSetLoading);
+
+		return () => {
+			router.events.off('routeChangeComplete', unSetLoading);
+			router.events.off('routeChangeError', unSetLoading);
+		};
+	}, [router]);
+
+	return loading ? <div>Loading...</div> : <Component {...pageProps} />
 };
 
 export default MyApp;
