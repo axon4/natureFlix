ed1eb123fa4e5834a9c232f775f55278ee3edb84
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-08 19:30:21 +0000

    set-up modal on video-page

--- pages/video/[ID].jsx
+++ pages/video/[ID].jsx
@@ -1,11 +1,29 @@
 import { useRouter } from 'next/router';
+import Modal from 'react-modal';
+import styles from '../../styles/VideoPage.module.css';
+
+Modal.setAppElement('#__next');
 
 function VideoPage() {
 	const router = useRouter();
 
 	return (
 		<main>
-			Video Page ID Query: {router.query.ID}
+			<Modal
+				className={styles.modal}
+				overlayClassName={styles.overlay}
+				isOpen={true}
+				onRequestClose={() => {router.back()}}
+			>
+				<iframe
+					className={styles.iFrame}
+					src={`https://www.youtube.com/embed/J01qyM3Y9VQ?autoplay=0&controls=0&rel=0&modestbranding=1`}
+					type='text/html'
+					frameBorder='0'
+					width='100%'
+					height='360'
+				/>
+			</Modal>
 		</main>
 	);
 };
