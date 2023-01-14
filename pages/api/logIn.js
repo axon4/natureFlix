3b405705db29027eef961baa62749eb971e41047
axon4 <65050809+axon4@users.noreply.github.com>
2023-01-14 20:40:34 +0000

    create JWT-token and new-user--query with Magic--server-SDK

--- pages/api/logIn.js
+++ pages/api/logIn.js
@@ -1,10 +1,30 @@
-function logIn(request, response) {
+import JWT from 'jsonwebtoken';
+import magicServer from '../../lib/magic/server';
+import { isNewUser } from '../../srv/hasura';
+
+async function logIn(request, response) {
 	switch (request.method) {
 		case 'POST': {
 			try {
 				const token = request.headers.authorization?.substr(7);
+				const metaData = await magicServer.users.getMetadataByToken(token);
+				const JWTToken = JWT.sign(
+					{
+						iat: Math.floor(Date.now() / 1000),
+						exp: Math.floor((Date.now() / 1000) + (7 * 24 * 60 * 60)),
+						"https://hasura.io/jwt/claims": {
+						  "x-hasura-allowed-roles": ["user", "admin"],
+						  "x-hasura-default-role": "user",
+						  "x-hasura-user-id": `${metadata.issuer}`,
+						},
+						...metaData
+					},
+					process.env.HASURA_JWT_SECRET
+				);
 
-				response.status(418).send(token);
+				const isThisNewUser = await isNewUser(token, metaData.issuer);
+
+				response.status(418).send({ JWTToken, isThisNewUser });
 			} catch (error) {
 				response.status(500).send('500 Internal Server Error:', error);
 			};
--- srv/hasura/index.js
+++ srv/hasura/index.js
@@ -1,10 +1,11 @@
-export async function graphQL(operation, operationName = '', variables = {}) {
+export async function graphQL(token, operation, operationName = '', variables = {}) {
 	try {
 		const response = await fetch(process.env.HASURA_GRAPHQL_URL, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
-				'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET
+				// 'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET,
+				Authorization: `Bearer ${token}`
 			},
 			body: JSON.stringify({
 				query: operation,
@@ -21,5 +22,25 @@ export async function graphQL(operation, operationName = '', variables = {}) {
 		return data;
 	} catch (error) {
 		console.error('Error Fetching GraphQL Data:', error);
+	};
+};
+
+export async function isNewUser(token, issuer) {
+	try {
+		const operation = `
+			query isNewUser($issuer: String!) {
+				users(where: {issuer: {_eq: $issuer}}) {
+					ID
+					eMail
+					issuer
+				}
+			}
+		`;
+
+		const response = await graphQL(token, operation, 'isNewUser', { issuer });
+		
+		return response?.data?.users?.length === 0;
+	} catch (error) {
+		console.error('Error Determining New User Status:', error);
 	};
 };
